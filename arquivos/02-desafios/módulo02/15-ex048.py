c3cfab3d272cf50f9bfee10b6eb35ba66d0f8dc9
João Gomes <joaoeduardo.gomes21@gmail.com>
2021-08-25 19:29:28 -0300

    modificações nos exercícios

--- arquivos/02-desafios/módulo02/15-ex048.py
+++ arquivos/02-desafios/módulo02/15-ex048.py
@@ -1,6 +1,7 @@
 # Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
 
+str(input("Quer saber todos os números ímpares múltiplos de 3 entre 1 e 500? Aperte ENTER que eu lhe moestro. "))
+
 for contador in range (3, 500, 3):
     if contador % 2 != 0:
         print(contador)
-        
--- arquivos/02-desafios/módulo02/17-ex050.py
+++ arquivos/02-desafios/módulo02/17-ex050.py
@@ -1,28 +1,6 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 # Dá pra fazer que, se o número for ímpar, o valor da variável dele é 0, e depois mandar somar tudo.
 
-"""n1 = int(input("Digite um número inteiro: "))
-n2 = int(input("Digite um número inteiro: "))
-n3 = int(input("Digite um número inteiro: "))
-n4 = int(input("Digite um número inteiro: "))
-n5 = int(input("Digite um número inteiro: "))
-n6 = int(input("Digite um número inteiro: "))
-
-if n1 % 2 != 0:
-    n1 = 0
-if n2 % 2 != 0:
-    n2 = 0
-if n3 % 2 != 0:
-    n3 = 0
-if n4 % 2 != 0:
-    n4 = 0
-if n5 % 2 != 0:
-    n5 = 0
-if n6 % 2 != 0:
-    n6 = 0
-
-print(f"A soma dos números pares é: {n1+n2+n3+n4+n5+n6}")"""
-
 lista = []
 
 for contador in range (1, 7):
