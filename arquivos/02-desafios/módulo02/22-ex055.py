abd1b73f489a46f7b00ab6e804345de682289175
João Gomes <joaoeduardo.gomes21@gmail.com>
2021-08-25 20:03:39 -0300

    resolução de exercícios

--- arquivos/02-desafios/módulo02/22-ex055.py
+++ arquivos/02-desafios/módulo02/22-ex055.py
@@ -1,1 +1,9 @@
 # Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+
+lista_pesos = []
+
+for contador in range (1, 6):
+    peso = float(input("Informe seu peso: Kg "))
+    lista_pesos.append(peso)
+
+print(f"O maior peso informado foi: {max(lista_pesos)}Kg")
--- arquivos/02-desafios/módulo02/23-ex056.py
+++ arquivos/02-desafios/módulo02/23-ex056.py
@@ -2,3 +2,23 @@
 - A média de idade do grupo
 - Qual o nome do homem mais velho
 - Quantas mulheres têm menos de 20 anos"""
+
+from statistics import mean #importando o cálculo de média
+
+media_idade = []
+homem_mais_velho = []
+mulheres_20 = []
+
+for contador in range (1, 5):
+    nome = str(input("Informe seu nome: ")).strip().title()
+    idade = int(input("Informe sua idade: "))
+    sexo = str(input("Qual seu sexo? (M/F) ")).strip().lower()
+    media_idade.append(idade)
+    if (sexo != 'm' and sexo != 'f') and (sexo != 'masculino' and sexo != 'feminino'):
+        print("Alternativa de sexo inválida.")
+
+    if (sexo == 'f' or sexo == 'feminino') and idade < 20:
+        mulheres_20.append(idade)
+
+print(f"A media da idade do grupo é {mean(media_idade):.2f}")
+print(f"O número de mulheres com menos de 20 anos é: {len(mulheres_20)}")
