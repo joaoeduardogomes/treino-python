ab93665db92c5d487837aeaf5398511286b83138
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2021-11-02 10:14:01 -0300

    lesson 20 - exercise
    ex100

--- arquivos/02-desafios/módulo03/74-ex100.py
+++ arquivos/02-desafios/módulo03/74-ex100.py
@@ -1,3 +1,25 @@
 """
 Faça um programa que tenha uma LISTA chamada NÚMEROS e duas FUNÇÕES chamadas SORTEIA() e SOMApAR(). A primeira função vai sortear 5 NÚMEROS e vai colocá-los dentro da lista. A segunda função vai mostrar a SOMA entre todos os valores PARES sorteados pela função anterior.
 """
+
+from random import randint
+
+def sorteia():
+    print('-=' * 20)
+    for cont in range(0, 5):
+        numeros.append(randint(1, 20))
+    print(f"Os números sorteados foram: {sorted(numeros)}")
+    print('-=' * 20)
+
+def somaPar():
+    print('=-' * 20)
+    soma = sum(num for num in numeros if num%2==0) # Esta parte vai somar só os pares, isto é, aqueles que tem resto 0 na divisão inteira por 2.
+    #original era: "sum(num for num in numeros if num%2==0)"
+    print(f"A soma de todos os números pares da lista é: {soma}")
+    print('=-' * 20)
+
+
+numeros = []
+
+sorteia()
+somaPar()
