51f28b5a240bba35cee67170e5d2594f3c7e4aea
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2021-11-22 15:24:26 -0300

    ex102

--- arquivos/02-desafios/módulo03/77-ex102.py
+++ arquivos/02-desafios/módulo03/77-ex102.py
@@ -3,3 +3,28 @@ Crie um programa que tenha uma função 'fatorial()' que receba dois parâmetros
 
 *dica de como fazer: 'print(fatorial(5, show=True)'
 """
+
+from math import factorial
+
+def fatorial(numero, show):
+    fatorial_num = factorial(entrada_num)
+    print(f"O fatorial de {entrada_num}! é {fatorial_num}")
+
+    if show == True:
+        print(f"O fatorial de {entrada_num}! em detalhes é: {entrada_num}", end="")
+        for cont in range(entrada_num-1, 0, -1):
+            print(f" x {cont}", end="")
+        print(f" = {fatorial_num}")
+
+
+entrada_num = int(input("Digite um número para saber seu fatorial: "))
+
+escolha = "a"
+while escolha not in 'sn':
+    escolha = str(input(f"Deseja ver o fatorial de {entrada_num} em detalhes? (s/n) ")).strip().lower()[0]
+    if escolha == 's':
+        mostrar = True
+    elif escolha == 'n':
+        mostrar = False
+
+fatorial(entrada_num, mostrar)
