1ffe820e7c3d86c7c2ea72e2ed8db3a1c9fbca39
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-01-20 20:32:33 -0300

    ex115 done

--- arquivos/CeV/módulo03/desafios_modulos_pacotes/ex115/arquivo.py
+++ arquivos/CeV/módulo03/desafios_modulos_pacotes/ex115/arquivo.py
@@ -1,6 +1,8 @@
+from ex115 import *
+
 def arquivo_existe(nome):
     try:
-        a = open(nome, 'rt')
+        a = open(nome, encoding='utf-8', mode='rt')
         a.close()
     except FileNotFoundError:
         return False
@@ -9,9 +11,34 @@ def arquivo_existe(nome):
 
 def cria_arquivo(nome):
     try:
-        a = open(nome, 'wt+')
+        a = open(nome, encoding='utf-8', mode='wt+')
         a.close()
     except:
         print("Houve um erro na criação do arquivo")
     else:
         print(f"Arquivo {nome} criado com sucesso.")
+
+def le_arquivo(nome):
+    try:
+        arquivo = open(nome, encoding='utf-8', mode='rt')
+    except:
+        print("Erro na leitura do arquivo.")
+    else:
+        cabecalho("PESSOAS CADASTRADAS")
+        print(arquivo.read())
+    finally:
+        arquivo.close()
+
+def cadastra(arquivo, nome = 'desconhecido', idade = 0):
+    try:
+        arquivo = open(arquivo, encoding='utf-8', mode='at')
+    except:
+        print("ERRO! O arquivo não pôde ser aberto.")
+    else:
+        try:
+            arquivo.write(f"{nome}, {idade}\n")
+        except:
+            print("Erro no cadastro de dados.")
+        else:
+            print(f"{nome} cadastrado(a) com sucesso!")
+            arquivo.close()
--- arquivos/CeV/módulo03/desafios_modulos_pacotes/ex115/teste.py
+++ arquivos/CeV/módulo03/desafios_modulos_pacotes/ex115/teste.py
@@ -1,24 +1,28 @@
 from ex115 import *
 from arquivo import *
+from inteiro import *
 from time import sleep
 
 print()
 
 arquivo = 'cadastro.txt'
 
-if arquivo_existe(arquivo):
-    print("Aqrquivo encontrado!")
-else:
-    print("Arquivo não encontrado!")
+if not arquivo_existe(arquivo):
+    cria_arquivo(arquivo)
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Realizar novo cadastro', 'Encerrar programa'])
     
     #A resposta recebe o valor 'n' retornado pelo 'inteiro.py'
     if resposta == 1:
-        print('Opção 1')
+        le_arquivo(arquivo)
+        sleep(1)
     elif resposta == 2:
-        print('Opção 2')
+        cabecalho('NOVO CADASTRO')
+        nome = str(input("Nome: "))
+        idade = leiaInt("Idade: ")
+        cadastra(arquivo, nome, idade)
+        sleep(1)
     elif resposta == 3:
         print("Encerrando o programa...")
         sleep(0.5)
