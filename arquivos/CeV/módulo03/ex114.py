39b479eef06f7c80597b0ed93f29c8f3a8f67284
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-01-20 17:39:14 -0300

    add new exercises
    113, 114, 115

---
+++ arquivos/CeV/módulo03/ex114.py
@@ -0,0 +1,5 @@
+'''
+Crie um código em Python que teste se o site Pudim está acessível pelo computador usado.
+
+** usar outro site, pois o antivírus diz que o site não é seguro
+'''
---
+++ arquivos/CeV/módulo03/ex115.py
@@ -0,0 +1,6 @@
+'''
+Crie um pequeno sistema modularizado que permite cadastrar pessoas pelo seu nome e idade em um arquivo de texto simples.
+O sistema só vai ter 2 opções: 
+    1) cadastrar uma nova pessoa
+    2) listar todas as pessoas cadastradas
+'''
