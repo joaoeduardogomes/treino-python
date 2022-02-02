a30c9765c0642b2580cf2e3580ddf1bd46e62d97
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-02-02 16:37:02 -0300

    finished URL info extraction

--- arquivos/03 Extraindo informações de uma URL/extrator_url.py
+++ arquivos/03 Extraindo informações de uma URL/extrator_url.py
@@ -1,3 +1,5 @@
+import re
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -12,6 +14,12 @@ class ExtratorURL:
     def valida_url(self):
         if not self.url: #é o mesmo que: if self.url == "". ele faz um 'bool(self.url)'
             raise ValueError("A URL está vazia")
+
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(self.url)
+
+        if not match:
+            raise ValueError("A URL não é válida!")
 
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
@@ -34,8 +42,46 @@ class ExtratorURL:
 
         return valor
 
+    def __len__(self):
+        return len(self.url)
 
-extrator_url = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar")
+    def __str__(self):
+        return self.url + "\n" + f"Parâmetros: {self.get_url_parametros()}" + "\n" + f"URL base: {self.get_url_base()}"
+
+    def __eq__(self, other):
+        return self.url == other.url
+
+
+url = "bytebank.com/cambio?quantidade=500&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
+extrator_url2 = ExtratorURL(url)
 
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade) 
+
+print(f"O tamanho da URL é: {len(extrator_url)}")
+
+print(extrator_url)
+
+print(extrator_url == extrator_url2)
+
+print(id(extrator_url))
+print(id(extrator_url2))
+
+
+
+### DESAFIO ###
+# Conversão de dólar para real
+VALOR_DOLAR = 5.50  # 1 dólar = 5.50 reais
+moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
+moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
+quantidade = extrator_url.get_valor_parametro("quantidade")
+
+if moeda_origem == "real" and moeda_destino == "dolar":
+    valor_conversao = int(quantidade) / VALOR_DOLAR
+    print(f"O valor de R$ {quantidade} reais é igual a ${valor_conversao} dólares.")
+elif moeda_origem == "dolar" and moeda_destino == "real":
+    valor_conversao = int(quantidade) * VALOR_DOLAR
+    print(f"O valor de $ {quantidade} dólares é igual a R$ {valor_conversao} reais.")
+else:
+    print(f"Câmbio de {moeda_origem} para {moeda_destino} não está disponível.")
--- arquivos/03 Extraindo informações de uma URL/validador_url.py
+++ arquivos/03 Extraindo informações de uma URL/validador_url.py
@@ -20,4 +20,11 @@ Exemplos de URLs inválidas:
 
 import re
 
-padrao_url = '(https://)'
+url = 'https://www.bytebank.com.br/cambio'
+padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+match = padrao_url.match(url)
+
+if not match:
+    raise ValueError("A URL não é válida!")
+else:
+    print("A URL é válida")
