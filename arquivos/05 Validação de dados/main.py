f97ab08dd4f17784c58d4e325efebb2e0b1841da
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-02-09 21:59:51 -0300

    validating data - finished lesson 1

---
+++ arquivos/05 Validação de dados/main.py
@@ -0,0 +1,17 @@
+from validate_docbr import CPF
+from cpf import Cpf
+
+print()
+
+#cpf = "78965412352" # precisa ser 'str' porque 'int' não tem 'len'
+
+#objeto_cpf = Cpf(cpf)
+
+'''cpf = CPF()
+
+print(cpf.validate("789.654.123-52"))'''
+
+cpf_um = Cpf('02549088026')
+print(cpf_um)
+
+print()
